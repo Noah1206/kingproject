2a9acaf1ebdfd383b81e35f99b3d1b63f586f24f
Noah1206 <89563822+Noah1206@users.noreply.github.com>
2025-05-19 15:35:27 +0900

    pass

--- home.js
+++ home.js
@@ -4,19 +4,36 @@ import { StyleSheet, Text, View, Image } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <Image style={styles.logo} source={require("./asset/logo.png")}></Image>
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: "bold",
+            gap: 20,
+          }}
+        >
+          할 일을 적고, 어서 시작해보세요!
+        </Text>
         <View style={styles.loginTitle}>
           <Text
             style={{
               color: "#ffffff",
-              textAlign: "center",
+              fontSize: 20,
             }}
           >
             로그인
           </Text>
         </View>
-        <Text style={styles.signTitle}>회원가입</Text>
+        <View style={styles.signTitle}>
+          <Text style={{ color: "#ffffff", fontSize: 20 }}>회원가입</Text>
+        </View>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -30,28 +47,36 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   loginTitle: {
+    flex: 0.1,
     backgroundColor: "#000000",
-    fontSize: 30,
-    fontWeight: "bold",
-    color: "#000",
-    marginTop: 0,
-    width: 100,
-    height: 40,
-    borderRadius: 20,
-    paddingBottom: 20,
-    alignItems: "center",
-  },
-  signTitle: {
-    fontSize: 15,
     fontWeight: "bold",
     textAlign: "center",
     color: "#000",
-    marginTop: 0,
-    paddingBottom: 20,
+    width: 250,
+    height: 20,
+    justifyContent: "center",
+    borderRadius: 50,
+    alignItems: "center",
+    marginTop: 55,
+  },
+  signTitle: {
+    flex: 0.1,
+    backgroundColor: "#000000",
+    fontSize: 15,
+    fontWeight: "bold",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 250,
+    height: 20,
+    borderRadius: 50,
+    textAlign: "center",
+    color: "#000",
+    marginTop: 20,
   },
   logo: {
+    flex: 0.3,
     width: 160,
     height: 100,
-    marginBottom: 20,
+    marginTop: 25,
   },
 });
